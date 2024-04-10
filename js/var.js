df411308a1ae61ff0504e35499efd979c41536da
정삼복 <68502584+3boku@users.noreply.github.com>
2024-04-10 21:25:29 +0900

    Create var.js

---
+++ js/var.js
@@ -0,0 +1,23 @@
+var name = "정현서";
+var age = 19;
+var gender = true;
+var height = 181.1;
+var job = undefined;
+var boyfriend = null;
+
+console.log(name);
+console.log(age);
+console.log(gender);
+console.log(height);
+console.log(job);
+console.log(boyfriend);
+
+console.log(
+  "제 이름은",
+  name,
+  "이고, 제 나이는",
+  age,
+  "입니다. 또한 제 키는",
+  height,
+  "입니다.",
+);
